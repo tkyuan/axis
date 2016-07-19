d441948da27ee9b7417474439f18462a4ef30ce4
tengkai.yuan <tengkai.yuan@dianping.com>
2016-07-19 18:06:09 +0800

    空闲心跳检测

--- axis/src/main/java/org/axisframework/axis/rpc/netty/client/NettyClientHandler.java
+++ axis/src/main/java/org/axisframework/axis/rpc/netty/client/NettyClientHandler.java
@@ -3,9 +3,14 @@
  */
 package org.axisframework.axis.rpc.netty.client;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.CharsetUtil;
 
 import org.axisframework.axis.model.AXSResponse;
 import org.slf4j.Logger;
@@ -19,6 +24,9 @@ import org.slf4j.LoggerFactory;
 public class NettyClientHandler extends SimpleChannelInboundHandler<AXSResponse>{
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(NettyClientHandler.class);
+	
+	private static final ByteBuf HEARTBEAT_SEQUENCE = Unpooled.unreleasableBuffer(
+            Unpooled.copiedBuffer("HEARTBEAT", CharsetUtil.ISO_8859_1));
 
 	private NettyClientFactory factory;
 
@@ -50,6 +58,20 @@ public class NettyClientHandler extends SimpleChannelInboundHandler<AXSResponse>
 	}
 
 
+
+	@Override
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt)
+			throws Exception {
+		if (evt instanceof IdleStateEvent) {
+			if(LOGGER.isInfoEnabled()){
+				LOGGER.info("netty-client send heartbeat...");
+			}
+            ctx.writeAndFlush(HEARTBEAT_SEQUENCE.duplicate())
+                    .addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+       } else {
+           super.userEventTriggered(ctx, evt);
+       }
+	}
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
--- axis/src/main/java/org/axisframework/axis/rpc/netty/client/NettyClientPipelineFactory.java
+++ axis/src/main/java/org/axisframework/axis/rpc/netty/client/NettyClientPipelineFactory.java
@@ -3,14 +3,19 @@
  */
 package org.axisframework.axis.rpc.netty.client;
 
+
+import java.util.concurrent.TimeUnit;
+
 import org.axisframework.axis.model.AXSRequest;
 import org.axisframework.axis.model.AXSResponse;
 import org.axisframework.axis.rpc.coder.RpcDecoder;
 import org.axisframework.axis.rpc.coder.RpcEncoder;
+
 import io.netty.channel.ChannelInboundHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
 
 
 /**
@@ -30,6 +35,7 @@ public class NettyClientPipelineFactory extends ChannelInitializer<SocketChannel
 		ChannelPipeline pipeline = ch.pipeline();
 		pipeline.addLast(new RpcEncoder(AXSRequest.class));
         pipeline.addLast(new RpcDecoder(AXSResponse.class));
+        pipeline.addLast(new IdleStateHandler(0, 0, 27, TimeUnit.SECONDS));
         pipeline.addLast(handler);
 	}
 
